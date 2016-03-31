e24d35ac99ff0eb0923fc6bb00938259357bb1ae
nazzzco <blacksin@abv.bg>
2016-03-31 20:10:21 +0300

    adding homework and demos

---
+++ MathManipulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Abs of the number -7 is {0}", Math.Abs(-7));
+        Console.WriteLine("The bigger number between 3 and 23 is {0}", Math.Max(3, 23));
+        Console.WriteLine("Square of 10 is {0}", Math.Sqrt(10));
+        Console.WriteLine("E is {0}", Math.E);
+
+        int width = 10;
+        int height = 4;
+        int area = (width * height) / 2;
+        Console.WriteLine("The area of triangle with width = 10 and height = 4 is {0}", area);
+    }
+}
---
+++ SquareRoot/Square.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Square
+    {
+        static void Main()
+        {
+            Console.WriteLine("The square root of the number 12345 is {0}", Math.Sqrt(12345));
+        }
+    }
+}
